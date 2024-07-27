b9efd2ec6091b972ae182605ae03a84cae3b6f5e
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2024-07-27 12:00:44 +0100

    Seperated each layers out to individual components and component files.

--- edge-clothing/src/App.js
+++ edge-clothing/src/App.js
@@ -1,43 +1,10 @@
-import './categories.styles.scss'
+import CategoryMenu from './components/category-menu/category-menu.component'
 
 const App = () => {
-
-
-  const categories = [
-    {
-      id: 1,
-      name: 'Hats',
-    },
-    {
-      id: 2, 
-      name: 'Jackets',
-    },
-    {
-      id: 3, 
-      name: 'Sneakers',
-    }, 
-    {
-      id: 4,
-      name: 'Womens',
-    }, 
-    {
-      id: 5, 
-      name: 'Mens',
-    }
-  ]
-
   return (
-    <div className="outerContainer">
-      {categories.map(({ name, id }) => (
-      <div key={ id } className="categoryContainer">
-        <div className='' />
-        <div className="categoryBodyContainer">
-          <h2>{ name }</h2>
-          <p>Shop Now!</p>
-        </div>
-      </div>
-      ))}
-    </div>
+   <div className=''>
+    <CategoryMenu />
+   </div>
   );
 }
 
@@ -49,50 +16,6 @@ export default App;
 // const App = () => {
 //   return (
 //     <div>Hello World</div>
-//   );
-// }
-
-// export default App;
-
-// ITERATING OVER AN ARRAY TO RENDER MULTIPLE COMPONENTS 
-
-// const App = () => {
-
-//   const categories = [
-//     {
-//       id: 1,
-//       name: 'Hats',
-//     },
-//     {
-//       id: 2, 
-//       name: 'Jackets',
-//     },
-//     {
-//       id: 3, 
-//       name: 'Sneakers',
-//     }, 
-//     {
-//       id: 4,
-//       name: 'Womens',
-//     }, 
-//     {
-//       id: 5, 
-//       name: 'Mens',
-//     }
-//   ]
-
-//   return (
-//     <div className="outerContainer">
-//       {categories.map(({name}) => (
-//       <div className="categoryContainer">
-//         {/* {img} */}
-//         <div className="categoryBodyContainer">
-//           <h2>{name}</h2>
-//           <p>Shop Now!</p>
-//         </div>
-//       </div>
-//       ))}
-//     </div>
 //   );
 // }
 
