d449c579ce53663710bfa4091f6d99ed5eac0f9a
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2024-07-27 12:09:38 +0100

    Handed down props on the categories

--- edge-clothing/src/App.js
+++ edge-clothing/src/App.js
@@ -1,10 +1,41 @@
 import CategoryMenu from './components/category-menu/category-menu.component'
 
+const categories = [
+  {
+    "id": 1,
+    "title": "Hats",
+    "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+  },
+  {
+    "id": 2,
+    "title": "Jackets",
+    "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+  },
+  {
+    "id": 3,
+    "title": "Sneakers",
+    "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+  },
+  {
+    "id": 4,
+    "title": "Womens",
+    "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+  },
+  {
+    "id": 5,
+    "title": "Mens",
+    "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+  },
+  // Storing the images in the array allows the image to be stored in a database and directly rendered from there 
+];
+
+// ITERATING OVER ITEMS IN AN ARRAY ALLOWS THE DEVELOPER TO RENDER MULTIPLE COMPONENTS WITH MINIMAL LINES OF CODE 
+// ALLOWING THE REUSE OF BASE CODE. LOGIC ABOVE WOULD BE CONNECTING TO A DATABASE 
+
 const App = () => {
   return (
-   <div className=''>
-    <CategoryMenu />
-   </div>
+    // Passing the categories array as a prop to the component, passing it down the tree
+    <CategoryMenu categories={categories} />
   );
 }
 
--- edge-clothing/src/components/category-menu/category-menu.component.jsx
+++ edge-clothing/src/components/category-menu/category-menu.component.jsx
@@ -2,39 +2,10 @@ import CategoryItem from '../category-items/category-item.component'
 
 import './category-menu.styles.scss'
 
-const categories = [
-  {
-    "id": 1,
-    "title": "Hats",
-    "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-  },
-  {
-    "id": 2,
-    "title": "Jackets",
-    "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-  },
-  {
-    "id": 3,
-    "title": "Sneakers",
-    "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-  },
-  {
-    "id": 4,
-    "title": "Womens",
-    "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-  },
-  {
-    "id": 5,
-    "title": "Mens",
-    "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-  },
-  // Storing the images in the array allows the image to be stored in a database and directly rendered from there 
-];
+// categories prop is the categories array. IE the database where the categories are stored.
+// being passed down from the main app (top of the tree)
 
-// ITERATING OVER ITEMS IN AN ARRAY ALLOWS THE DEVELOPER TO RENDER MULTIPLE COMPONENTS WITH MINIMAL LINES OF CODE 
-// ALLOWING THE REUSE OF BASE CODE. LOGIC ABOVE WOULD BE CONNECTING TO A DATABASE 
-
-const CategoryMenu = () => {
+const CategoryMenu = ({ categories }) => {
   return (
     <div className="categories-container">
      {categories.map((category) => (
