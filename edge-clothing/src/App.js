2384577352494b24e3b06ded20f2a3c681695d7f
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2024-07-27 11:03:02 +0100

    Created initial Reactapp for Edge-Clothing store. Added a basic web app to display each category card

---
+++ edge-clothing/src/App.js
@@ -0,0 +1,96 @@
+const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      name: 'Hats',
+    },
+    {
+      id: 2, 
+      name: 'Jackets',
+    },
+    {
+      id: 3, 
+      name: 'Sneakers',
+    }, 
+    {
+      id: 4,
+      name: 'Womens',
+    }, 
+    {
+      id: 5, 
+      name: 'Mens',
+    }
+  ]
+
+  return (
+    <div className="outerContainer">
+      {categories.map(({name}) => (
+      <div className="categoryContainer">
+        <div className='' />
+        <div className="categoryBodyContainer">
+          <h2>{name}</h2>
+          <p>Shop Now!</p>
+        </div>
+      </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
+
+
+// BASIC REACT APP LAYOUT
+
+// const App = () => {
+//   return (
+//     <div>Hello World</div>
+//   );
+// }
+
+// export default App;
+
+// ITERATING OVER AN ARRAY TO RENDER MULTIPLE COMPONENTS 
+
+// const App = () => {
+
+//   const categories = [
+//     {
+//       id: 1,
+//       name: 'Hats',
+//     },
+//     {
+//       id: 2, 
+//       name: 'Jackets',
+//     },
+//     {
+//       id: 3, 
+//       name: 'Sneakers',
+//     }, 
+//     {
+//       id: 4,
+//       name: 'Womens',
+//     }, 
+//     {
+//       id: 5, 
+//       name: 'Mens',
+//     }
+//   ]
+
+//   return (
+//     <div className="outerContainer">
+//       {categories.map(({name}) => (
+//       <div className="categoryContainer">
+//         {/* {img} */}
+//         <div className="categoryBodyContainer">
+//           <h2>{name}</h2>
+//           <p>Shop Now!</p>
+//         </div>
+//       </div>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default App;
